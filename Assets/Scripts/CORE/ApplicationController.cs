854ad33a9d5e34b549fe76bbc66d79beb5b93140
p3v1Xamk <pekka.vilpponen@xamk.fi>
2025-04-18 06:28:04 +0300

    Change effects for testing.

--- Assets/Scripts/CORE/ApplicationController.cs
+++ Assets/Scripts/CORE/ApplicationController.cs
@@ -76,11 +76,11 @@ public class ApplicationController : SingletonMono<ApplicationController>
 
         demoEffects = new List<DemoEffectBase>()
         {
-            new DemoeffectIntro().Init(20f, "Press Space or Fire") ,
+            new DemoeffectIntro().Init(25f, "Press Space or Fire") ,
             //new DemoEffectRun().Init(20f, "Toggle left/right rapidly to run"),                        
             //new DemoeffectTextScroller().Init(20f, "Control ship with left/right and up/down.\nPress fire to shoot."),
             //new DemoEffectEyeBalls().Init(30f, "Left/right to control the ship.\nPress fire to shoot."),
-            new DemoeffectNoise().Init(20f, ""),
+            new DemoeffectNoise().Init(25f, ""),
             //new DemoEffectSunset().Init(30f, "Left/right to control the character. Press fire to shoot"),
             //new DemoEffectMatrix().Init(30f, "Left/right to control the hand.\nCatch highlighted falling letters"),
             //new DemoEffectTimeBomb().Init(30f, "Defuse the bomb")
